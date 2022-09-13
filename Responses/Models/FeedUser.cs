77dd03a9078e054cf5595ea7c76c5775f6cabf8c
Jan Husak <jan.husak@ysoft.com>
2022-09-13 19:04:19 +0200

    fix model

--- Responses/Models/FeedUser.cs
+++ Responses/Models/FeedUser.cs
@@ -9,7 +9,7 @@
 
         public string Name { get; set; }
         public string Path { get; set; }
-        public string Designation { get;set }
+        public string Designation { get; set; }
         public string LastActivity { get; set; }
         public FeedImage Image { get; set; }
     }
