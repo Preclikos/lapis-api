02104d40ff9455cf78bdd5f6700cc358657f1c3f
Jan Husak <jan.husak@ysoft.com>
2022-09-25 14:47:54 +0200

    modify location model

--- Database/Models/LapisLocation.cs
+++ Database/Models/LapisLocation.cs
@@ -3,8 +3,20 @@
     public class LapisLocation
     {
         public int Id { get; set; }
+        public int LapisId { get; set; }
         public decimal Lat { get; set; }
         public decimal Long { get; set; }
-        public int LapisId { get; set; }
+        public decimal Alt { get; set; }
+        public int CountryId { get; set; }
+        public string CountryCode { get; set; }
+        public string Country { get; set; }
+        public string PostCode { get; set; }
+        public string State { get; set; }
+        public string County { get; set; }
+        public string Municipality { get; set; }
+        public string City { get; set; }
+        public string Suburb { get; set; }
+        public string Road { get; set; }
+        public string HouseNumber { get; set; }
     }
 }
